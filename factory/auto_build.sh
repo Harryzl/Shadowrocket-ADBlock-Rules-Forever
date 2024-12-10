e9d01d5ab75eac176f96d2504b1baa609f3697f6
Harry <45171929+Harryzl@users.noreply.github.com>
2024-12-10 21:44:55 +0800

    Update auto_build.sh

--- factory/auto_build.sh
+++ factory/auto_build.sh
@@ -8,10 +8,7 @@ python3 gfwlist.py
 python3 build_confs.py
 python3 build_singbox_confs.py
 
-VERSION=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases/latest \
-| grep tag_name \
-| cut -d ":" -f2 \
-| sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
+VERSION=1.11.0-beta.8
 
 curl -Lo sing-box.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v${VERSION}/sing-box-${VERSION}-linux-amd64.tar.gz"
 tar -xzvf sing-box.tar.gz
