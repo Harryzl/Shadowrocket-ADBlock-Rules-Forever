5d97c501e36ce81965057f1e653e05fc9e923883
Harry <45171929+Harryzl@users.noreply.github.com>
2024-12-10 21:38:28 +0800

    Update auto_build.sh

--- factory/auto_build.sh
+++ factory/auto_build.sh
@@ -7,3 +7,15 @@ python3 ad.py
 python3 gfwlist.py
 python3 build_confs.py
 python3 build_singbox_confs.py
+
+VERSION=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases/latest \
+| grep tag_name \
+| cut -d ":" -f2 \
+| sed 's/\"//g;s/\,//g;s/\ //g;s/v//')
+
+curl -Lo sing-box.tar.gz "https://github.com/SagerNet/sing-box/releases/download/v${VERSION}/sing-box-${VERSION}-linux-amd64.tar.gz"
+tar -xzvf sing-box.tar.gz
+mv ./sing-box-${VERSION}-linux-amd64/sing-box .
+chmod +x sing-box
+
+./sing-box rule-set compile --output ../figure/proxy_list.srs ../proxy_list.json
